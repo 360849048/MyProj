c523acf474ffdf8e6b6a43630ef84c0bcde55ac3
13555 <13555/HAITIAN>
2018-07-25 14:01:05 +0800

    在index.html中直接引用font-awesome。如果使用webpack打包font-awesome，build.js文件会增大约1.4M

--- web/src/components/iomaker/FuncConfig.vue
+++ web/src/components/iomaker/FuncConfig.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="funcconfig">
     <h1>Function should be configured here</h1>
+    <div class="list-group">
+      <a class="list-group-item" href="#"><i class="fa fa-home fa-fw"></i>&nbsp; Home</a>
+      <a class="list-group-item" href="#"><i class="fa fa-book fa-fw"></i>&nbsp; Library</a>
+      <a class="list-group-item" href="#"><i class="fa fa-pencil fa-fw"></i>&nbsp; Applications</a>
+      <a class="list-group-item" href="#"><i class="fa fa-cog fa-fw"></i>&nbsp; Settings</a>
+    </div>
   </div>
 </template>
 
